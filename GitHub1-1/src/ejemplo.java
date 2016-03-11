da01c3ac1aef084d39ad65870c542489524a575d
Elliot APS <Elliot APS@Elliot>
2016-03-11 18:23:02 +0000

    Change-Id: I9db46d93b83dc35d52c7132f15a68d9227f61428 Signed-off-by: Elliot APS <Elliot APS@Elliot>

---
+++ GitHub1-1/src/ejemplo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ejemplo {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner (System.in);
+		String name = "abc";
+		
+		
+		System.out.println("Plese, What your name?");
+		name = keyboard.nextLine();
+		System.out.println("You name is: " + name);
+		
+		System.out.println("Okey, My name is John");
+		
+		System.out.println("Guay, nos echamos un LOL?");
+		
+	}
+
+}
